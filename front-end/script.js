1d2ba8faa2583d0f37269e9b168770180798bea0
yokino-dev <lannyke7@gmail.com>
2025-12-09 17:48:42 -0300

    HTML server

--- front-end/script.js
+++ front-end/script.js
@@ -4,16 +4,21 @@ const UL_LIST = document.getElementById ("task-list");
 const TASK_INPUT = document.getElementById ("tasklist-input");
 const SAVE_TASK = document.getElementById ("save-tasklist")
 
-
-async function fetchtasks() {
-// 1. Faz a requisição e espera a resposta
-    const response = await fetch('http://localhost:8000/tasks');
-// 2. Extrai os dados Json e espera o processamento
-    const task = await response.json()
-
+function showTasks(tasks){
+    UL_LIST.innerHTML = '';
 }
 
-await fetch('http://localhost:8000/tasks'), {
-    method: 'POST', 
+async function fetchtasks_and_showTasks() {
+// 1. Faz a requisição e espera a resposta
+    const response = await fetch('http://localhost:8000/tasks')
     
+// 2. Tratamento de Erro ao Servidor   
+    if (!response.ok) {
+        throw new Error("Ocorreu um Erro HTTP \n Status do Erro: ${response.status}");
+    }
+// 3. Extrai os dados Json e espera o processamento
+    const task = await response.json()
+
+// 4. Exibe no Site as tarefas
+    showTasks(tasks);
 }
